0338d021ec20f187617bf9dfb95014b06be75178
hytham defrawy <hythamdefrawy@gmail.com>
2021-11-10 15:28:55 -0500

    Create routesand changes to view files

--- express_server.js
+++ express_server.js
@@ -2,10 +2,37 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const cookieParser = require('cookie-parser')
+app.use(cookieParser())
+
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true})); 
 
 app.set("view engine", "ejs")
+
+
+
+// res.render("urls_index", templateVars);
+
+app.get('/login', (req, res) => {
+res.render('_header') 
+});
+
+app.post('/login', (req, res) => {
+  console.log(req.body)
+  const username = req.body.username;
+  res.cookie('username', username);
+res.redirect('/urls')
+});
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('username')
+res.redirect('/urls')
+});
+
+
+
 
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
@@ -21,7 +48,7 @@ const urlDatabase = {
   })
 
   app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
+    const templateVars = { urls: urlDatabase, username: req.cookies['username'] };
   res.render("urls_index", templateVars);
 })
 
@@ -36,23 +63,37 @@ app.post("/urls", (req, res) => {
 
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
-  const longURL = urlDatabase[shortURL].longURL;
+  console.log(shortURL)
+  console.log(urlDatabase)
+  const longURL = urlDatabase[shortURL];
+  console.log(longURL)
   res.redirect(longURL);
 });
-
+/////////////****** *
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.body.longURL]};
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls/:shortURL/delete", (req, res) =>{
 
+// UPDATE => update the info in the db
+app.post('/urls/:shortURL', (req, res) => {
+  // extract the id
+  const shortURL = req.params.shortURL;
+  // extract the question and anwer
+  const longURL = req.body.longURL;
+  // update the db
+  urlDatabase[req.params.shortURL] = longURL
+  // redirect
+  res.redirect('/urls');
+
+})
+
+app.post("/urls/:shortURL/delete", (req, res) =>{
   // extract the id
   const shortURL = req.params.shortURL;
   // delete this joke from db
   delete urlDatabase[req.params.shortURL];
-
-  res.redirect('/jokes')
   res.redirect("/urls");
 })
 
